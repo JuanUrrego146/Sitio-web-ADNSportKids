0f43f70071c85ce7b06786753d27456651dd5c0b
AndrexS05 <est.andres.sgutier1@unimilitar.edu.co>
2025-10-11 09:31:09 -0500

    Catalogo_Maqueta

--- js/Catalogo_java_script.js
+++ js/Catalogo_java_script.js
@@ -1,7 +1,7 @@
 const productos=[
     {Categoria:"Hombre", Sub_Categoria:"Uniformes",Nombre:"Uniforme inter de Miami",imagen:"Imagenes/Uni_Inter_Miami_Frontal.png",precio:"$85.0000"},
     {Categoria:"Hombre", Sub_Categoria:"Uniformes",Nombre:"Uniforme Real Madrid",imagen:"Imagenes/Uni_Real_Madrid.png",precio:"$85.0000"},
-    {Categoria:"Hombre", Sub_Categoria:"Uniformes",Nombre:"Uniforme Real Madrid",imagen:"Imagenes/camisa-front.avif",precio:"$80.0000"},
+    {Categoria:"Hombre", Sub_Categoria:"Uniformes",Nombre:"Uniforme Colombia",imagen:"Imagenes/camisa-front.avif",precio:"$80.0000"},
     {Categoria:"Mujer", Sub_Categoria:"Bolsos",Nombre:"Mochila deportiva Gris Nike",imagen:"Imagenes/Mochila_Gris_Nike.png",precio:"$85.0000"},
     {Categoria:"Mujer", Sub_Categoria:"Bolsos",Nombre:"Nike Morral Gym Amarillo",imagen:"Imagenes/Morral_Gym_Amarillo.png",precio:"$85.0000"},
     {Categoria:"Hombre", Sub_Categoria:"Bolsos",Nombre:"Maleta deportiva adidas",imagen:"Imagenes/Maleta_Deportiva_Adidas.png",precio:"$85.0000"},
@@ -15,4 +15,47 @@ const productos=[
     {Categoria:"N/A", Sub_Categoria:"Balones",Nombre:"Pelota Tennis Wilson",imagen:"Imagenes/Pelota_Tennis_Wilson.png",precio:"$15.000"},
     {Categoria:"N/A", Sub_Categoria:"Balones",Nombre:"Balon de Voleiboyl by Golty",imagen:"Imagenes/Balon_Voleibol_Golty.png",precio:"$85.000"},
 ]
+const catalogo = document.getElementById("catalogo");
+
+
+function mostrarCatalogo(categoriaSeleccionada = null){
+    catalogo.innerHTML =""; //Limpia el html
+
+    
+
+    productos.forEach(p=> {
+
+        const divProducto = document.createElement("div");
+        divProducto.classList.add("Producto");
+
+        const divImagen=document.createElement("div");
+        divImagen.classList.add("producto-imagen");
+
+        const img=document.createElement("img");
+        img.src=p.imagen;
+        img.alt=p.Nombre;
+        divImagen.appendChild(img);
+
+        const divInfo =document.createElement("div");
+        divInfo.classList.add("producto-info");
+
+        const nombre = document.createElement("h3");
+        nombre.classList.add("producto-nombre");
+        nombre.textContent = p.Nombre;
+
+        const precio = document.createElement("p");
+        precio.classList.add("producto-precio");
+        precio.textContent = p.precio;
+
+        divInfo.append(nombre,precio);
+        divProducto.append(divImagen,divInfo);
+        catalogo.appendChild(divProducto);
+
+    });
+
+
+}
+
+mostrarCatalogo();
+
 
